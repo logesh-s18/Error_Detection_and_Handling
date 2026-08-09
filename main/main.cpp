648421c3bba51723231e7de3229bc2dc4fa29405
Logeshwaran1821 <logeshwaransivasankar@gmail.com>
2026-08-10 01:14:40 +0530

    Add project files.

---
+++ main/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+// We want to test the following function
+// For simplicity, we'll ignore that 'y' is sometimes counted as a vowel
+bool isLowerVowel(char c)
+{
+    switch (c)
+    {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+        return true;
+    default:
+        return false;
+    }
+}
+
+int main()
+{
+    // So here's our temporary tests to validate it works
+    std::cout << isLowerVowel('a') << '\n'; // temporary test code, should produce 1
+    std::cout << isLowerVowel('q') << '\n'; // temporary test code, should produce 0
+
+    return 0;
+}
